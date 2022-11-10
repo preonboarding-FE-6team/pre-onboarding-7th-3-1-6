40793f6025d12567ef9c35d254d677fbb19f902f
2ujin <suddenlydisappear200@gmail.com>
2022-11-11 00:57:03 +0900

    chore: build test

--- src/api/suggestion.ts
+++ src/api/suggestion.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const SERVER = process.env.REACT_APP_SERVER;
 
 export const getSuggestions = async (param: string) => {
+  console.log();
   const { data } = await axios.get<Sick[]>(`${SERVER}/sick?sickNm_like=${param}`);
   return data;
 };
