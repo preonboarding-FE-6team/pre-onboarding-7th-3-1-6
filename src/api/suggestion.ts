8a9b3949d9086330e41532808b68f1495e047eae
younhoso <thdbsgh3443@naver.com>
2022-11-11 01:01:54 +0900

    Feat: Error state & Error message Add

--- src/api/suggestion.ts
+++ src/api/suggestion.ts
@@ -1,8 +1,30 @@
 import axios from 'axios';
 
-const SERVER = process.env.REACT_APP_SERVER;
+// const SERVER = process.env.REACT_APP_SERVER;
+
+const SERVER = {
+  baseURL: process.env.REACT_APP_SERVER,
+  headers: { 'Content-Type': 'application/json' },
+};
+
+const api = axios.create(SERVER);
+
+api.interceptors.request.use(
+  (config) => config,
+  () => ({ message: '런타임 에러가 발생했습니다!' })
+);
+api.interceptors.response.use(
+  (config) => config,
+  (error) => error.response
+);
 
 export const getSuggestions = async (param: string) => {
-  const { data } = await axios.get<Sick[]>(`${SERVER}/sick?sickNm_like=${param}`);
-  return data;
+  const { data, status } = await api.get<Sick[]>(`${SERVER.baseURL}/sick?sickNm_like=${param}`);
+  return {
+    data,
+    status,
+    errorMsg: {
+      default: '데이터를 불러오는데 실패하였습니다!',
+    },
+  };
 };
